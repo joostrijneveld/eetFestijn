d1310e5e2102dc16b478a3f8bfc0ae2608f1e385
Joost Rijneveld <joostrijneveld@gmail.com>
2015-03-26 18:03:48 +0100

    Added PoC index.html that lists items

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from orders.models import Item
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    item_list = Item.objects.all()
+    template = loader.get_template('orders/index.html')
+    context = RequestContext(request, {
+        'item_list': item_list,
+    })
+    return HttpResponse(template.render(context))
