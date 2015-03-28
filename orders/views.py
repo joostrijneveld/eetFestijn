b960825d88a7779781bd8f02117ff346888e9559
Joost Rijneveld <joostrijneveld@gmail.com>
2015-03-28 01:05:53 +0100

    Actually store the orders in the database

--- orders/views.py
+++ orders/views.py
@@ -3,13 +3,22 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 
-from orders.models import Item
+from orders.models import Item, Order, ItemOrder
 from orders.forms import OrderForm
 
 def index(request):
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
+            order = Order()
+            order.name = form.cleaned_data['name']
+            order.in_wie_betaalt_wat = form.cleaned_data['wiebetaaltwat']
+            order.save()  # need to save to get an id, to prevent ValueError
+            item_ids = request.POST.getlist('items[]')
+            for item_id in item_ids:
+                item = Item.objects.get(pk=item_id)
+                order_item = ItemOrder.objects.create(item=item, order=order)
+            order.save()
             return HttpResponseRedirect(reverse('orders:success'))
     else:
         form = OrderForm()
