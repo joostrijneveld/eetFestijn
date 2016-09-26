7413af9beeb2c70d36473cb94cfd6d62d3170ab3
Joost Rijneveld <joost@joostrijneveld.nl>
2016-09-26 09:30:11 +0200

    Prevent curl breaking on 301

--- orders/urls.py
+++ orders/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^summary\.pdf$', views.summary_PDF, name='summary_PDF'),
     url(r'^overview/$', views.overview, name='overview'),
     url(r'^receipts/$', views.receipts, name='receipts'),
-    url(r'^print/$', views.print_script, name='print_script'),
+    url(r'^print/?$', views.print_script, name='print_script'),
 ]
